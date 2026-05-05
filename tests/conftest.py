1c1f49380773914b03e8da036bca85dd833fc63e
Karl Hans Laurits <karl@laurits.dev>
2026-05-05 13:04:13 +0300

    test: isolate config state

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
 import os
+import tempfile
+from pathlib import Path
 
 from hypothesis import settings
+
+_TEST_CONFIG_DIR = Path(tempfile.mkdtemp(prefix="sambatui-tests-"))
+os.environ["SAMBATUI_USER_CONFIG"] = str(_TEST_CONFIG_DIR / "config.toml")
+os.environ["SAMBATUI_PASSWORD_FILE"] = str(_TEST_CONFIG_DIR / "password")
+os.environ.pop("SAMBATUI_PASSWORD", None)
+
 
 settings.register_profile(
     "sambatui",
